c9f54c31328fb80c69433f5fcfc5db4df0b30015
Artem Kikhtenko <artemka81@gmail.com>
2017-08-26 23:57:01 +0300

    Add profile-add

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,24 @@ $('.nav-mobile').on('click', function(){
 	$('#wrapper-nav.wrapper-nav--open').removeClass('wrapper-nav--open');
 })
 
+/* Upload*/
+var inputs = document.querySelectorAll( '.inputfile' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+	var label	 = input.nextElementSibling,
+		labelVal = label.innerHTML;
+
+	input.addEventListener( 'change', function( e )
+	{
+		var fileName = '';
+		if( this.files && this.files.length > 1 )
+			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+		else
+			fileName = e.target.value.split( '\\' ).pop();
+
+		if( fileName )
+			label.querySelector( 'span' ).innerHTML = fileName;
+		else
+			label.innerHTML = labelVal;
+	});
+});
